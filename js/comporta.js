5dcdfca57e72445cc66014b38ebece4e7abfd16f
CristianTafur249 <cristian2191970@correo.uis.edu.co>
2021-09-22 06:43:35 -0500

    Update comporta.js

--- js/comporta.js
+++ js/comporta.js
@@ -2,7 +2,18 @@ function guardar() {
     /* console.log('Dentro de guardar') */
     var dato = document.getElementById('gustos').value
     console.log(dato)
-    document.set
+    if(dato!=""){    var respu =document.getElementById('res')
+    respu.innerHTML =""
+    var tabla ="<tr>" 
+    tabla += '<td id="gust">'
+    tabla += dato
+    tabla += '</td>'
+    tabla += '<td id="porcent"> 0 </td>'
+    tabla += '<td> <input id ="edit" type="button" onclick="editar()" value="editar">  </td>'
+    tabla += "</tr>"
+    respu.innerHTML =tabla
+}
+
 }
 function limpia(){
     document.getElementById('gustos').value= "" 
