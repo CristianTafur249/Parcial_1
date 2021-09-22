581ed60578f95b2b2848a4d47453bd05516a0da2
CristianTafur249 <cristian2191970@correo.uis.edu.co>
2021-09-21 21:43:43 -0500

    Update comporta.js

---
+++ js/comporta.js
@@ -0,0 +1,11 @@
+function guardar() {
+    /* console.log('Dentro de guardar') */
+    var dato = document.getElementById('gustos').value
+    console.log(dato)
+    document.set
+}
+function limpia(){
+    document.getElementById('gustos').value= "" 
+    console.log('limpia')
+    console.log(document.getElementById('gustos').value)
+}
